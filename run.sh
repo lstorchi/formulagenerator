614b0ef085d054afd8064c9ddaf2c074acaae533
Loriano Storchi <loriano@storchi.org>
2023-05-03 11:29:42 +0200

    backup

--- run.sh
+++ run.sh
@@ -27,31 +27,57 @@
 
 # 3 elements alla possible
 
+#export count=1
+#for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+#do
+#  for bf2  in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+#  do 
+#    if [ "$bf1" != "$bf2" ]; then
+#      for bf3 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+#      do 
+#        if [ "$bf3" != "$bf1" ]; then
+#          if [ "$bf3" != "$bf2" ]; then
+#            echo "Counter: " $count
+#            echo $bf1 $bf2 $bf3
+#           
+#            python3 ./generatefeats.py  -f cleandataset.xlsx -b "$bf1[1];$bf2[1];$bf3[1]" 
+#            python3 ffilter.py -u -f newadata.pkl -n 50 -i "./cleandataset.xlsx,Gexp,nonxtb"
+#            
+#            for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
+#            do 
+#               mv $name $name"."$count
+#            done
+#           
+#            count=$((count+1))
+#          fi
+#        fi
+#      done
+#    fi
+#  done
+#done
+
 export count=1
-for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+for bf1 in DG LE
 do
-  for bf2  in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+  for bf2 in HIEmligand
   do 
-    if [ "$bf1" != "$bf2" ]; then
-      for bf3 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3 mTDS numbtors HIEmligand HIEligandE logP
+    for bf3 in logP
+    do 
+      echo "Counter: " $count
+      echo $bf1 $bf2 $bf3
+
+      python3 ./generatefeats.py  -f cleandataset.xlsx -b "$bf1[1];$bf2[1];$bf3[1]" 
+      python3 ffilter.py -u -f newadata.pkl -n 50 -i "./cleandataset.xlsx,Gexp,xtb"
+      
+      for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
       do 
-	if [ "$bf3" != "$bf1" ]; then
-	  if [ "$bf3" != "$bf2" ]; then
-            echo "Counter: " $count
-            echo $bf1 $bf2 $bf3
-           
-            python3 ./generatefeats.py  -f cleandataset.xlsx -b "$bf1[1];$bf2[1];$bf3[1]" 
-            python3 ffilter.py -u -f newadata.pkl -n 50 -i "./cleandataset.xlsx,Gexp,nonxtb"
-            
-            for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
-            do 
-               mv $name $name"."$count
-            done
-           
-            count=$((count+1))
-	  fi
-        fi
+         mv $name $name"."$count
       done
-    fi
+
+      count=$((count+1))
+
+    done
   done
 done
+
+
