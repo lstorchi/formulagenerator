792dff3cb70509d5f7a418376155e569e54d2a67
Loriano Storchi <loriano@storchi.org>
2023-05-03 10:46:09 +0200

    add script for formula generator

---
+++ run.sh
@@ -0,0 +1,26 @@
+
+export count=1
+for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3
+do
+  for bf2  in mTDS numbtors
+  do 
+    for bf3 in HIEmligand HIEligandE logP
+    do 
+      echo "Counter: " $count
+      echo $bf1 $bf2 $bf3
+
+      python3 ./generatefeats.py  -f cleandataset.xlsx -b "$bf1[1];$bf2[1];$bf3[1]" 
+      python3 ffilter.py -u -f newadata.pkl -n 50 -i "./cleandataset.xlsx,Gexp,nonxtb"
+      
+      for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
+      do 
+         mv $name $name"."$count
+      done
+
+      count=$((count+1))
+
+    done
+  done
+done
+
+
