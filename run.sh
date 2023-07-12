82c3d82fc67020d8d09ca9b91aea1783df5a83f9
Loriano Storchi <loriano@storchi.org>
2023-07-12 13:30:55 +0200

    updated after discussion

--- run.sh
+++ run.sh
@@ -80,20 +80,45 @@
 #  done
 #done
 
+#export bf4="logP"
+#export count=1
+#for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3
+#do
+#  for bf2  in mTDS numbtors
+#  do 
+#    for bf3 in HIEmligand HIEligandE 
+#    do 
+#      echo "Counter: " $count
+#      echo $bf1 $bf2 $bf3 $bf4
+#
+#      python3 ./generatefeats.py -f cleandataset.xlsx --fourelementsformula -b "$bf1[1];$bf2[1];$bf3[1];$bf4[1]" 
+#      python3 ffilter.py -u -f newadata.pkl -i "./cleandataset.xlsx,Gexp,xtb"
+#
+#      for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
+#      do 
+#         mv $name $name"."$count
+#      done
+#
+#      count=$((count+1))
+#
+#    done
+#  done
+#done
+
 export count=1
-export bf4="logP"
-for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2 PIEmFMO3 FE3
+for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2
 do
-  for bf2  in mTDS numbtors
+  for bf2  in  mTDS numbtors
   do 
-    for bf3 in HIEmligand HIEligandE 
+    for bf3 in HIEmligand HIEligandE logP
     do 
-      echo "Counter: " $count
-      echo $bf1 $bf2 $bf3 $bf4
+      echo "FR3 Counter: " $count $bf1 $bf2 $bf3
 
-      python3 ./generatefeats.py -f cleandataset.xlsx --fourelementsformula -b "$bf1[1];$bf2[1];$bf3[1];$bf4[1]" 
-      python3 ffilter.py -u -f newadata.pkl -i "./cleandataset.xlsx,Gexp,xtb"
-
+      python3 ./generatefeats.py  -f cleandataset.xlsx \
+        -b "$bf1[1];$bf2[1];$bf3[1]" 
+      python3 ffilter.py -u -f newadata.pkl -n 50 \
+        -i "./cleandataset.xlsx,Gexp,nonxtb"
+      
       for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
       do 
          mv $name $name"."$count
@@ -105,4 +130,53 @@ do
   done
 done
 
+export bf4="logP"
+export count=1
+for bf1 in DEmFMO2 F2LE PIEmFMO2 FE2
+do
+  for bf2  in  mTDS numbtors
+  do 
+    for bf3 in HIEmligand HIEligandE
+    do 
+      echo "FR4 Counter: " $count $bf1 $bf2 $bf3 $bf4
 
+      python3 ./generatefeats.py --fourelementsformula -f cleandataset.xlsx \
+        -b "$bf1[1];$bf2[1];$bf3[1];$bf4[1]" 
+      python3 ffilter.py -u -f newadata.pkl -n 50 \
+        -i "./cleandataset.xlsx,Gexp,nonxtb"
+      
+      for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
+      do 
+         mv $name $name"."$count
+      done
+
+      count=$((count+1))
+
+    done
+  done
+done
+
+export bf4="dEpligand"
+for bf1 in PIEmFMO2 FE2
+do
+  for bf2  in  mTDS numbtors
+  do 
+    for bf3 in HIEmligand HIEligandE logP
+    do 
+      echo "FR4 Counter: " $count
+      echo $bf1 $bf2 $bf3
+
+      python3 ./generatefeats.py --fourelementsformula -f cleandataset.xlsx \
+        -b "$bf1[1];$bf2[1];$bf3[1];$bf4[1]" 
+      python3 ffilter.py -u -f newadata.pkl -n 50 \
+        -i "./cleandataset.xlsx,Gexp,nonxtb"
+      
+      for name in newadata.pkl finalformulalist.txt newadata.csv feature_mse.csv finalselectedformulas.txt
+      do 
+        mv $name $name"."$count
+      done
+
+      count=$((count+1))
+    done
+  done
+done
